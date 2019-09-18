7f5edc8a46192926f6cdb6be09ca733be187e151
Antoine GELLOZ <agelloz@e1r1p9.42.fr>
2019-09-18 13:32:06 +0200

    Beginning of parser

--- inc/lem_in.h
+++ inc/lem_in.h
@@ -6,13 +6,14 @@
 /*   By: agelloz <agelloz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/14 16:33:56 by agelloz           #+#    #+#             */
-/*   Updated: 2019/09/17 12:12:38 by agelloz          ###   ########.fr       */
+/*   Updated: 2019/09/18 13:28:45 by agelloz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEM_IN_H
 # define LEM_IN_H
 
+# include <stdlib.h>
 # include "libft.h"
 
 typedef struct	s_edge
@@ -34,8 +35,17 @@ typedef struct	s_node
 
 typedef struct	s_graph
 {
-	int		size;
+	size_t	size;
 	t_node	*array;
 }				t_graph;
+
+t_graph	*create_graph(size_t size);
+int8_t	add_edge(t_graph *graph, int src, int dest);
+
+t_graph	*exit_parse_error(void);
+t_graph *exit_graph_error(void);
+int8_t	exit_free_graph(t_graph *graph);
+
+void	print_graph(t_graph *graph);
 
 #endif
---
+++ src/exit.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agelloz <agelloz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/18 12:00:34 by agelloz           #+#    #+#             */
+/*   Updated: 2019/09/18 13:30:30 by agelloz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "lem_in.h"
+
+t_graph	*exit_parse_error(void)
+{
+	ft_putendl_fd("ERROR", 2);
+	return (NULL);
+}
+
+t_graph	*exit_graph_error(void)
+{
+	ft_putendl_fd("GRAPH ERROR", 2);
+	return (NULL);
+}
+
+int8_t	exit_free_graph(t_graph *graph)
+{
+	size_t	index;
+	t_edge	*temp;
+	t_edge	*node;
+
+	index = 0;
+	while (index < graph->size)
+	{
+		node = graph->array[index].head;
+		while (node != NULL)
+		{
+			temp = node;
+			node = node->next;
+			free(temp);
+			temp = NULL;
+		}
+		index++;
+	}
+	free(graph->array);
+	free(graph);
+	graph = NULL;
+	return (SUCCESS);
+}
