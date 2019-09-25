9c7ff91b1982af85a5a27b06502f850de4446071
ekelkel <elora.kelkel@gmail.com>
2019-09-25 18:38:08 +0200

    Get queue created

--- src/bfs.c
+++ src/bfs.c
@@ -6,8 +6,40 @@
 /*   By: ekelkel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 13:32:38 by ekelkel           #+#    #+#             */
-/*   Updated: 2019/09/25 15:47:58 by ekelkel          ###   ########.fr       */
+/*   Updated: 2019/09/25 18:37:45 by ekelkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "lem_in.h"
 
+int			get_queue(t_graph *graph)
+{
+	int		i;
+	t_queue *queue;
+	t_edge	*lst;
+
+	queue = create_queue(graph->size);
+	i = 0;
+	while (graph->array[i])
+	{
+		if (graph->array[i]->source == TRUE)
+		{
+			queue[0] = graph->array[i]->index;
+			break;
+		}
+		i++;
+	}
+
+	while (isEmpty(queue) == FALSE)
+	{
+		graph->array[i]->bfs_marked = TRUE;
+		dequeue(queue);
+		lst = graph->array[i]->head;
+		while (lst->next)
+		{
+			lst = lst->next;
+			enqueue(queue, lst->dest);
+		}
+		i = find(queue[queue->front], graph->array);
+	}
+}
