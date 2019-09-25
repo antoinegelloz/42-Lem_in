3641de2a65fd652af72dbe9c5dc6a44d36d5425f
ekelkel <elora.kelkel@gmail.com>
2019-09-25 18:44:04 +0200

    Get queue modification

--- src/bfs.c
+++ src/bfs.c
@@ -6,11 +6,25 @@
 /*   By: ekelkel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 13:32:38 by ekelkel           #+#    #+#             */
-/*   Updated: 2019/09/25 18:37:45 by ekelkel          ###   ########.fr       */
+/*   Updated: 2019/09/25 18:43:36 by ekelkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+static int	find_index(int data, int *array)
+{
+	int		i;
+
+	i = 0;
+	while (array[i])
+	{
+		if (array[i] == data)
+			break ;
+		i++;
+	}
+	return (i);
+}
 
 int			get_queue(t_graph *graph)
 {
@@ -40,6 +54,6 @@ int			get_queue(t_graph *graph)
 			lst = lst->next;
 			enqueue(queue, lst->dest);
 		}
-		i = find(queue[queue->front], graph->array);
+		i = find_index(queue[queue->front], graph->array);
 	}
 }
