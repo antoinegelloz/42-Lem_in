3c4ad1275abae33611eef56bdabf38773c37d7e1
agelloz <agelloz@student.42.fr>
2019-09-27 14:07:35 +0200

    parsing correction 2

--- src/graph.c
+++ src/graph.c
@@ -6,7 +6,7 @@
 /*   By: agelloz <agelloz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/14 16:33:35 by agelloz           #+#    #+#             */
-/*   Updated: 2019/09/27 14:01:29 by agelloz          ###   ########.fr       */
+/*   Updated: 2019/09/27 14:07:10 by agelloz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,7 @@ void	print_graph(t_graph *graph)
 	while (i < graph->size)
 	{
 		temp = graph->array[i].head;
-		//ft_printf("node|%d|, name|%s|, x|%d|, y|%d|", i,
-		//graph->array[i].name, graph->array[i].x_coord,
-		//graph->array[i].y_coord);
-		ft_printf("node|%d|", i);
+		ft_printf("Node|%d|, Name|%s|, x|%d|, y|%d|", i, graph->array[i].name, graph->array[i].x_coord, graph->array[i].y_coord);
 		while (temp)
 		{
 			ft_printf(" -> %d", temp->dest);
@@ -82,10 +79,13 @@ t_graph	*create_graph(t_parsing *p)
 		graph->array[i].name = ft_strdup(p->nodes->content);
 		graph->array[i].source = (p->source == i) ? 1 : 0;
 		graph->array[i].sink = (p->sink == i) ? 1 : 0;
-		graph->array[i].x_coord = *(int *)p->x_coord;
-		graph->array[i].y_coord = *(int *)p->y_coord;
+		graph->array[i].x_coord = *(int *)p->x_coord->content;
+		graph->array[i].y_coord = *(int *)p->y_coord->content;
 		graph->array[i].bfs_marked = 0;
 		graph->array[i].head = NULL;
+		p->nodes = p->nodes->next;
+		p->x_coord = p->x_coord->next;
+		p->y_coord = p->y_coord->next;
 		i++;
 	}
 	return (graph);
